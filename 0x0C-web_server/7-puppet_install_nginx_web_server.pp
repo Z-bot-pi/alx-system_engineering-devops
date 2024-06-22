66e13fdee147136054061a86c8c2aec100b9d471
Z-bot-pi <xulubanazintle@gmail.com>
2024-06-22 13:44:18 -0700

    0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,43 @@
-#automate my work with puppet
-
-pacckage {'nginx':
+# Install Nginx
+package { 'nginx':
   ensure => installed,
 }
 
-file_line {'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.github.com/z-bot-pi permanent;',
-}
-
+# Create the index.html file with "Hello World!" content
 file { '/var/www/html/index.html':
+  ensure  => file,
   content => 'Hello World!',
-}
-
-service {'nginx':
-  ensure  => running,
   require => Package['nginx'],
 }
+
+# Configure Nginx to listen on port 80 and add the 301 redirect
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Enable and start the Nginx service
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => File['/etc/nginx/sites-available/default'],
+}
+
+# Nginx configuration template (nginx/default.erb)
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
+  ensure => file,
+  content => @("END"),
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+            try_files $uri $uri/ =404;
+        }
